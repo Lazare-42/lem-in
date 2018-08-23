a1ad2e7523c48f4f2fe5c4ead4a5a2d9531dfab9
Lazare ROSSILLON <lazrossi@e1r3p1.42.fr>
2018-08-23 15:05:32 +0200

    maj visu et dprintf

--- visualisateur/test.py
+++ visualisateur/test.py
@@ -4,19 +4,22 @@ from pygame.locals import *
 
 pygame.init()
 
-#Ouverture de la fenetre Pygame
-fenetre = pygame.display.set_mode((640, 480))
+#Ouverture de la screen Pygame
+screen = pygame.display.set_mode((2560, 1440), RESIZABLE)
 
 #Chargement et collage du fond
 fond = pygame.image.load("background.png").convert()
+fond = pygame.transform.scale(fond, (2560, 1440), screen)
+#red = [244, 164, 96]
+#screen.fill(red)
 
-fenetre.blit(fond, (0,0))
+screen.blit(fond, (0,0))
 
 
 #Chargement et collage du personnage
-perso = pygame.image.load("perso.png").convert_alpha()
-position_perso = perso.get_rect()
-fenetre.blit(perso, position_perso)
+#perso = pygame.image.load("perso.png").convert_alpha()
+#position_perso = perso.get_rect()
+#screen.blit(perso, position_perso)
 
 
 #Rafraichissement cd l'ecran
@@ -31,12 +34,14 @@ while continuer:
     for event in pygame.event.get():    #Attente des events
         if event.type == QUIT:
             continuer = 0
-        if event.type == KEYDOWN:
-            if event.key == K_DOWN: #If down arrow
-                #On descend le perso
-                    position_perso = position_perso.move(0,3)
+#        if event.type == KEYDOWN:
+#            if event.key == K_DOWN:
+#                position_perso = position_perso.move(0,3)
+        if event.type == MOUSEBUTTONUP:
+            pygame.draw.circle(screen, [0, 0, 255], (10, 10), 0)
+    pygame.draw.circle(screen, [0, 0, 255], (10, 10), 0)
     #Re-collage
-    fenetre.blit(fond, (0,0))   
-    fenetre.blit(perso, position_perso)
+    screen.blit(fond, (0,0))   
+#    screen.blit(perso, position_perso)
     #refresh
     pygame.display.flip()
