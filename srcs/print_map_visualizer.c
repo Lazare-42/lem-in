219f9e4459f497c63437f0a8002755d58c086cb9
Lazare ROSSILLON <lazrossi@e2r8p21.42.fr>
2018-08-26 22:42:53 +0200

    visualizer can now read maps / show them / and modify them on the fly

--- srcs/print_map_visualizer.c
+++ srcs/print_map_visualizer.c
@@ -6,7 +6,7 @@
 /*   By: lazrossi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/25 19:44:21 by lazrossi          #+#    #+#             */
-/*   Updated: 2018/08/25 20:28:38 by lazrossi         ###   ########.fr       */
+/*   Updated: 2018/08/26 18:52:24 by lazrossi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,14 +25,13 @@ void	print_map_visualizer(t_info info)
 	i = 0;
 	fd = open("/Users/lazrossi/Documents/42/lem-in/output.map",
 			O_CREAT | O_RDWR | O_TRUNC, S_IRWXU);
-	ft_dprintf(fd, "%d\n", info.n);
 	while (i < info.n)
 	{
 		ft_dprintf(fd, "%s %d %d", info.nodelist[i].name, info.nodelist[i].x, info.nodelist[i].y);
 		j = 0;
 		while (j < info.n)
 		{
-			ft_dprintf(fd, " %d ", info.nodelist[i].tubes[j]);
+			ft_dprintf(fd, " %d", info.nodelist[i].tubes[j]);
 			j++;
 		}
 		ft_dprintf(fd, "\n");
